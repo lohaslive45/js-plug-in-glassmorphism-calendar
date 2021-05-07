23e551773c02ac8f5645fc3ab42bc121f4843027
lohaslive45 <74390305+lohaslive45@users.noreply.github.com>
2021-05-07 12:06:05 +0800

    上傳

---
+++ main.js
@@ -0,0 +1,17 @@
+dycalendar.draw({
+    target:'#dycalendar',
+    type:'month',
+    dayformat:'full',
+    monthformat:'full',
+    highlighttargetdate:'true',
+    prevnextbutton:'show'
+})
+
+//*---API請參考原作者 https://github.com/yusufshakeel/dyCalendarJS ---
+//*---簡易功能介紹---------------------------------------------------
+//*---target---顯示今日 (星期/日/月/年)---
+//*---type---月曆/日曆 (1)month月 (2)day日
+//*---dayformat---星期完整/縮寫 (1)full完整顯示 <Sunday> (2)ddd簡略顯示 <Sun>
+//*---monthformat---月份完整/縮寫 (1)full完整顯示 <April> (2)mmm簡略顯示 <Apr>
+//*---highlighttargetdate---凸顯選中日期 (1)ture開啟 (2)false關閉
+//*---prevnextbutton---顯示前後月按鈕 (1)show開啟 (2)hide隱藏
